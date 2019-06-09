b6835e151a550823cafc830801221e0066541bd4
Roman <islam.roman92@gmail.com>
2019-06-09 11:20:29 -0400

    Here we go

---
+++ PracticeG/src/GitPractice.java
@@ -0,0 +1,11 @@
+
+public class GitPractice {
+
+	public static void main(String[] args) {
+		System.out.println("My Name Is Roman Islam");
+		System.out.println("This is my second attempt");
+
+
+	}
+
+}
